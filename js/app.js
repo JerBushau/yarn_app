6ae0049f01a0ea8ff55cb5dcda7f065478a9f56f
Jeremiah Bushau <exjerlock@hotmail.com>
2016-12-24 07:20:56 -0500

    whitespace

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+let n = 1;
+
 // bootstrap dropdown active class bug workaround
 $('.nav-pills').on('shown.bs.tab', 'a', function(e) {
     if (e.relatedTarget) {
@@ -7,18 +9,13 @@ $('.nav-pills').on('shown.bs.tab', 'a', function(e) {
     }
 });    
 
-let n = 1;
-
 document.getElementById('addProjectButton').onclick = function(e) {
 	let addInput = document.getElementById('addProjectInput');
 	let title = addInput.value;
 	let a = new Display(n, title);
 
 	e.preventDefault();
-
 	a.render();
-	
 	n++;
-
 	addInput.value = '';
 };
