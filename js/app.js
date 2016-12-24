bf6404b62bde56cb9e1509003aeb9a22d277e49f
Jeremiah Bushau <exjerlock@hotmail.com>
2016-12-24 13:10:08 -0500

    styled bootstrap UI components

--- js/app.js
+++ js/app.js
@@ -9,13 +9,28 @@ $('.nav-pills').on('shown.bs.tab', 'a', function(e) {
     }
 });    
 
+
+
 document.getElementById('addProjectButton').onclick = function(e) {
 	let addInput = document.getElementById('addProjectInput');
-	let title = addInput.value;
+	let title = addInput.value.trim();
 	let a = new Display(n, title);
+
+	if (!title) {
+		return false
+	}
 
 	e.preventDefault();
 	a.render();
 	n++;
 	addInput.value = '';
+
+	if (document.getElementById('nav').children.length > 0) {
+		document.getElementById('drpdwn').style.display = 'block';
+	}
+	
+	$('.active').removeClass('active');
+	$('#' + a.title + a.num).tab('show');
+	$('.dropdown-toggle').addClass('active');
+	$('.dropdown-item[href="#' + a.title + a.num + '"]').addClass('active');
 };
