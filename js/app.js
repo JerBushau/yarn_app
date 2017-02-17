a2f7b756c823cb2f8435ea6643704fade50e8a23
Jeremiah Bushau <exjerlock@hotmail.com>
2017-02-16 23:32:03 -0500

    Implemented local storage

--- js/app.js
+++ js/app.js
@@ -2,9 +2,25 @@
 
 const addInput = document.getElementById('addProjectInput');
 const addButton = document.getElementById('addProjectButton');
-const arrayOfProjects = [];
+const storage = Lockr.get('projects');
 
-let n = 1;
+let arrayOfProjects = [];
+let n = Lockr.get('n') || 0;
+
+if (storage) {
+  for (let i = 0; i < storage.length; i++) {
+    arrayOfProjects.push(storage[i]);
+    render(storage[i]);
+  }
+}
+
+function navLengthCheck() {
+  if (document.getElementById('nav').childElementCount) {
+    document.getElementById('drpdwn').style.display = 'block';
+  }
+}
+
+navLengthCheck();
 
 // stop submit when user presses enter while focused on input
 addInput.onkeypress = function(e) {
@@ -21,16 +37,77 @@ addButton.onclick = function(e) {
     return false
   }
 
-  e.preventDefault();
+  navLengthCheck();
+  console.log(document.getElementById('nav').childElementCount)
+
   arrayOfProjects.push(project);
-  project.render();
+
+  e.preventDefault();
+  render(project);
   n++;
 
-  if (document.getElementById('nav').children.length > 0) {
-    document.getElementById('drpdwn').style.display = 'block';
-  }
+  Lockr.set('n', n)
+  Lockr.set('projects', arrayOfProjects)
 
   addInput.value = '';
 
   $('a[href="#' + project.title + project.num + '"]').tab('show');
 };
+
+function render(obj) {
+  const mainContainer = document.getElementById('mainContainer');
+  const nav = document.getElementById('nav');
+  const widgetCons = mainContainer.getElementsByClassName('widgetContainer' + obj.num);
+
+  nav.insertAdjacentHTML('beforeend', obj.template.link);
+  mainContainer.insertAdjacentHTML('beforeend', obj.template.content);
+
+  for (let i = 0; i < widgetCons.length; i++) {
+    bindButtons(widgetCons[i], '_row', obj);
+    bindButtons(widgetCons[i], 'stitch', obj);
+  }
+}
+
+function bindButtons(el, prefix, obj) {
+  const main = el.querySelector('section.' + prefix);
+  const up = main.querySelector('button.' + prefix + 'Up');
+  const down = main.querySelector('button.' + prefix +'Down');
+  const clear = main.querySelector('button.' + prefix + 'Clear');
+  const number = main.querySelector('div.' + prefix + 'Number');
+  let prop;
+
+  let index = arrayOfProjects.indexOf(obj);
+
+  if (prefix === '_row') {
+    prop = obj._row;
+  } else if ( prefix === 'stitch') {
+    prop = obj.stitch;
+  } 
+  
+  number.innerText = prop;
+
+  up.onclick = () => {
+    prop++;
+    number.innerText = prop;
+    arrayOfProjects[index][prefix] = prop;
+    Lockr.set('projects', arrayOfProjects);
+  }
+
+  down.onclick = () => {
+    if (!prop) {
+      return
+    }
+
+    prop--;
+    number.innerText = prop;
+    arrayOfProjects[index][prefix] = prop;
+    Lockr.set('projects', arrayOfProjects);
+  }
+
+  clear.onclick = () => {
+    prop = 0;
+    number.innerText = prop;
+    arrayOfProjects[index][prefix] = prop;
+    Lockr.set('projects', arrayOfProjects);
+  }
+}
--- js/project.js
+++ js/project.js
@@ -1,84 +1,9 @@
 'use strict'
 
-function Project(x, title) {
-  this.num = x;
+function Project(num, title) {
+  this.num = num;
   this.title = title.replace(/\s+/g, '-');
-  this.row = new Counter();
-  this.stitch = new Counter();
-
-  this.tabLinkHtml = `
-  <a class="dropdown-item" data-toggle="pill" href="#${this.title}${this.num}" 
-  role="tab">${title}</a>`; 
-
-  this.tabContentHtml = ` 
-  <div class="widgetContainer${this.num} tab-pane fade" id="${this.title}${this.num}" role="tabpanel">
-    <h1 class="project-title">${title}</h1>
-    <section class="_row">
-      <h1 class="title">row</h1>
-      <br>
-      <button class="_rowClear clear title">clear</button>
-      <div class="display">
-        <button class="_rowDown arrow"><</button>
-        <div class="_rowNumber">
-        </div>
-        <button class="_rowUp arrow">></button>
-      </div>
-    </section>
-    <section class="stitch">
-      <h1 class="title">stitch</h1>
-      <br>
-      <button class="stitchClear clear title">clear</button>
-      <div class="display">
-        <button class="stitchDown arrow"><</button>
-        <div class="stitchNumber">
-        </div>
-        <button class="stitchUp arrow">></button>
-      </div>
-    </section>
-  </div>`;
-}
-
-Project.prototype.render = function() {
-  const mainContainer = document.getElementById('mainContainer');
-  const nav = document.getElementById('nav');
-  const widgetCons = mainContainer.getElementsByClassName('widgetContainer' + this.num);
-
-  nav.insertAdjacentHTML('beforeend', this.tabLinkHtml);
-  mainContainer.insertAdjacentHTML('beforeend', this.tabContentHtml);
-
-  for (let i = 0; i < widgetCons.length; i++) {
-    this.bindButtons(widgetCons[i], '_row');
-    this.bindButtons(widgetCons[i], 'stitch');
-  }
-}
-
-Project.prototype.bindButtons = function(el, pre) {
-  const main = el.querySelector('section.' + pre);
-  const up = main.querySelector('button.' + pre + 'Up');
-  const down = main.querySelector('button.' + pre +'Down');
-  const clear = main.querySelector('button.' + pre + 'Clear');
-  const number = main.querySelector('div.' + pre + 'Number');
-
-  if (pre === '_row') {
-    pre = this.row;
-  } else if ( pre === 'stitch') {
-    pre = this.stitch;
-  } 
-
-  number.innerText = pre.currentNumber;
-
-  up.onclick = () => {
-    pre.up();
-    number.innerText = pre.currentNumber;
-  }
-
-  down.onclick = () => {
-    pre.down();
-    number.innerText = pre.currentNumber;
-  }
-
-  clear.onclick = () => {
-    pre.clear();
-    number.innerText = pre.currentNumber;
-  }
+  this._row = 0;
+  this.stitch = 0;
+  this.template = new Template(this, title);
 }
